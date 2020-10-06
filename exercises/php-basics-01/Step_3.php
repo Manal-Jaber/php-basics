a633dfeb5efc357b7b744aab9d6008935c496d68
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-06 13:09:36 +0300

    Completed php-basics-01: Step_3

---
+++ exercises/php-basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Manal";
+$email = "manal.jaber94@gmail.com";
+echo "My name is ".$name." and my email is ".$email."\n";
+$height= 170;
+$width= 80;
+$area= $height * $width;
+echo $area;
+?>
